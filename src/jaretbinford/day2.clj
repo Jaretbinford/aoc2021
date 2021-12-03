fe4638b3c45959eaa4b0926c2de374fbbd003f37
Jaret Binford <jaretbinford@gmail.com>
2021-12-03 07:27:42 -0500

    part2 day 2

--- src/jaretbinford/day2.clj
+++ src/jaretbinford/day2.clj
@@ -31,6 +31,23 @@
   ;1654760
   )
 
+(defn part-2
+  [all-instructions]
+  (loop [x 0
+         y 0
+         aim 0
+         instructions all-instructions]
+    (if (empty? instructions)
+      (* x y)
+      (let [[k v] (first instructions)
+            dx (if (= k :forward) v 0)
+            dy (if (= k :forward) (* v aim) 0)
+            daim (if (= k :down) v (if (= k :up) (- v) 0))]
+        (recur (+ x dx) (+ y dy) (+ aim daim) (rest instructions))))))
+
+(comment
+  (part-2 parsed-input))
+
 
 
 
